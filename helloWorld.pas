ece551defefa4d47801efa4e585524f3f0a23ea2
Liz Kerber <emkerber@outlook.com>
2017-03-03 18:52:22 -0600

    Initial commit - Delphi Hello World

---
+++ helloWorld.pas
@@ -0,0 +1,35 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.Windows.fmx MSWINDOWS}
+{$R *.Surface.fmx MSWINDOWS}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+   Label1.Text := 'Hello World';
+end;
+
+end.
